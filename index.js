559d3c85f3ed4f2e7efe83a52eaa8336b4cc1b50
nadunc <djnadunc@gmail.com>
2017-04-26 21:28:17 +0530

    Template direcory changed

--- index.js
+++ index.js
@@ -10,13 +10,31 @@ app.use(bodyParser.json());
 app.use('/app', express.static(__dirname + '/public'));
 
 
+mongoose.connect('mongodb://localhost:27017/pharmacy', err => {
+    if (err) {
+        console.log(err);
+        // process.exit(1);
+    }
+});
+
+
+
 app.get('/', (req, res) => {
     //res.sendFile(__dirname + '/public/index.html');
     res.redirect('/app');
 });
 
-app.get('/app', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+// app.get('/app', (req, res) => {
+//     res.sendFile(__dirname + '/public/index.html');
+// });
+
+//
+// app.get('/app/*', (req, res) => {
+//     res.sendFile(__dirname + '/public/login.html');
+// });
+
+app.get('/app/login', (req, res) => {
+    res.sendFile(__dirname + '/public/login.html');
 });
 
 app.get('/app/starter', (req, res) => {
