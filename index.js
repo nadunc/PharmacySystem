9e1d66240b24eec009c52b116c5430b6f6a43db6
nadunc <djnadunc@gmail.com>
2017-04-26 20:23:55 +0530

    AngularJS added

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+'use strict';
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/app', express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+    //res.sendFile(__dirname + '/public/index.html');
+    res.redirect('/app');
+});
+
+app.get('/app', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+
+
+app.listen(3000, err => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('app listening on port 3000');
+});
