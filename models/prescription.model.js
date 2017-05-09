20f9b1b2ca77a6d97910637ffca0efaa999c047c
Dasuni Perera <das.perera@yahoo.com>
2017-05-09 13:43:31 +0530

    Update prescription.model.js

--- models/prescription.model.js
+++ models/prescription.model.js
@@ -21,7 +21,44 @@ const PrescriptionSchema = new Schema({
     patientID: {
         type: String,
         required: true
+    },
+    
+    druglist: {
+        
+            
+
+             description: {
+                type: String,
+                required: true
+            },
+
+            dosage: {
+                type: String,
+                required: true
+            },
+
+            frequency: {
+                type: String,
+                required: true
+            },
+
+            period: {
+                type: String,
+                required: true
+            },
+
+            quantity: {
+                type: String,
+                required: true
+            }
+            
+
     }
+
+   
+
+
+
 
 
 });
