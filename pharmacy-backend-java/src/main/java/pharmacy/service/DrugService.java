75970caf16158a67e9f44a6f6db47a226e10f437
nadunc <djnadunc@gmail.com>
2017-07-02 22:01:01 +0530

    Update

--- pharmacy-backend-java/src/main/java/pharmacy/service/DrugService.java
+++
@@ -1,7 +0,0 @@
-package pharmacy.service;
-
-/**
- * Created by Nadun on 6/28/2017.
- */
-public interface DrugService {
-}
--- pharmacy-frontend/assets/js/menu.js
+++
@@ -1,5 +0,0 @@
-$('.sidebar-menu li a').click(function () {
-    $('.sidebar-menu li').removeClass('active');
-    $(this).parent().addClass('active');
-});
-
